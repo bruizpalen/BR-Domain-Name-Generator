ac3fc18eb6177d8b49f56439f8a7339251ea29c3
bruizpalen <115789234+bruizpalen@users.noreply.github.com>
2023-07-20 18:48:44 +0000

    First version with 5 domains randomly selected

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function getFiveRandomeIndexesOfAnArray(array) {
+  const indexes = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  while (indexes.length < 5) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    if (!indexes.includes(randomIndex)) {
+      indexes.push(randomIndex);
+    }
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return indexes;
+}
+
+function createString() {
+  let domains = [];
+  let pron = ["the", "our"];
+  let adjec = ["great", "big"];
+  let name = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+  for (let i = 0; i < pron.length; i++) {
+    for (let j = 0; j < adjec.length; j++) {
+      for (let k = 0; k < name.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          domains.push(`${pron[i]}${adjec[j]}${name[k]}${extension[l]}`);
+        }
+      }
+    }
+  }
+
+  const domainsElement = document.getElementById("domains");
+  domainsElement.innerHTML = ""; // Clear the previous content
+  const randomNumbers = getFiveRandomeIndexesOfAnArray(domains);
+  for (const number of randomNumbers) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = domains[number];
+    domainsElement.appendChild(paragraph);
+  }
+  // domains.forEach((domain) => {
+  //   const paragraph = document.createElement("p");
+  //   paragraph.textContent = domain;
+  //   excuseElement.appendChild(paragraph);
+  // });
+  // const excuseElement = document.getElementById("excuse");
+  const generateButton = document.getElementById("generateButton");
+  const reloadButton = document.getElementById("reloadButton");
+  // excuseElement.innerText = result;
+  reloadButton.classList.remove("hidden");
+}
